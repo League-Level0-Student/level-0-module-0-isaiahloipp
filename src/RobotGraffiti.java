ce4ad4d0ffe068927108e4d3b635459edcf3f20d
isaiah <isaiahloipp@gmail.com>
2019-09-10 17:54:33 -0700

    Robot Graffiti

---
+++ src/RobotGraffiti.java
@@ -0,0 +1,18 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotGraffiti {
+public static void main(String[] args) {
+	Robot Isaiah=new Robot();
+Isaiah.penDown();
+Isaiah.move(60);
+Isaiah.turn(90);
+Isaiah.penDown();
+Isaiah.move(60);
+Isaiah.turn(90);
+Isaiah.move(120);
+
+
+
+
+}
+}
--- src/_01_robot_square/RobotSquare.java
+++ src/_01_robot_square/RobotSquare.java
@@ -11,23 +11,23 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
-    	// 1. Make a new Robot
-
-
+    	// 1. Make a new robot
+Robot Isaiah=new Robot();
         // 3. Put the robot's pen down
-
+Isaiah.penDown();
 
         // 6. Make the robot move as fast as possible
-
-
+Isaiah.setSpeed(400);
+int sides=0;
         // 5. Do everything below here 4 times
-
+while(sides<4) {
 
         //         2. Move your robot 200 pixels
-
+Isaiah.move(200); 
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
-
-
+Isaiah.turn(90);
+sides=sides+1;
     }
+}
 }
